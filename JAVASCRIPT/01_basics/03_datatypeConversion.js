b36e14795c5ded7e4e189a42c62b9a727deb6dd2
Aditya Meena <adityameena2233@gmail.com>
2025-05-17 12:49:03 +0530

    dataatypes conversion

---
+++ JAVASCRIPT/01_basics/03_datatypeConversion.js
@@ -0,0 +1,64 @@
+let score = "aditya";
+
+// console.log(typeof score);
+// console.log(typeof(score));
+
+let ValueinNumber = Number(score);
+// console.log(typeof ValueinNumber);
+// console.log(ValueinNumber);
+
+// 33 => 33
+// "33abc" => NaN
+// true => 1; false => 0
+
+let isLoggeedIn = 0;
+
+let booleanISloggedIn = Boolean(isLoggeedIn);
+// console.log(booleanISloggedIn)
+
+// 1 => true; 0 => false
+// "" => false
+//
+
+let somenumber = 33;
+
+let stringNumber = String(somenumber);
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+
+//***************Operations**************
+
+let value = 3;
+let negValue = -value;
+//console.log(negValue)
+
+// console.log(2+2);
+// console.log(2*2);
+// console.log(2-2);
+// console.log(2**3);
+// console.log(2/2);
+// console.log(2%2);
+
+let str1 = "hello";
+let str2 = " huppyyy";
+let str3 = str1 + str2;
+
+// console.log(str3);
+
+// console.log("1" + 2);
+
+// console.log(1 + "2");
+
+// console.log("1" + 2 + 2);
+
+// console.log(1 + 2 + "2");
+
+// console.log(true);
+
+let num1, num2,  num3
+
+num1=num2=num3 = 2+2
+
+let gameCounter = 100
+gameCounter++;
+console.log(gameCounter);
