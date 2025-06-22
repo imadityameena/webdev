3abf48c0f2086e128beb3768569c963d1eaa6386
Aditya Meena <adityameena2233@gmail.com>
2025-06-22 12:56:06 +0530

    learned about how to creater jsx

--- REACT/01vitereact/src/App.jsx
+++ REACT/01vitereact/src/App.jsx
@@ -3,12 +3,14 @@ import Coffee from "./coffee"
 
 
 function App() {
+  const username = "chai or code"
  
 
   return (
     <>
    <Coffee/>
-   <h1>Coffee or  react </h1>
+   <h1>Coffee or  react {username} </h1>
+   <p>test para</p>
    </>
   )
     
