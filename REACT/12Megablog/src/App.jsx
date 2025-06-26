50baa3f1d27169291141e4d8441257d565210a81
Aditya Meena <adityameena2233@gmail.com>
2025-06-26 20:56:19 +0530

    authoriazation from appwrite done

---
+++ REACT/12Megablog/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+
+import './App.css'
+
+function App() {
+ console.log(import.meta.env.VITE_APPWRITE_URL);
+ 
+
+  return (
+    <>
+      <h1>A Blog  App in AppWrite</h1>
+    </>
+  )
+}
+
+export default App
